ef8d456e47219c140e9549e060ae07484e9728dd
lauradacol <laura.dacol@gmail.com>
2017-08-29 15:55:05 -0300

    Update piscaleds1.v
    tentativa de usar o botão

--- piscaleds1.v
+++ piscaleds1.v
@@ -1,13 +1,34 @@
 module piscaleds1(
 	input CLOCK_50,
-	output [5:0]LEDG
+	input [3:0] KEY,
+	output [7:0]LEDG
 );
 
 reg [26:0]contador = 0;
 reg l = 0;
+reg l1 = 1;
+reg l3 = 0;
 
-assign LEDG = l;
+assign LEDG[1] = l1; 
+assign LEDG[2] = l;
+assign LEDG[3] = l1; 
+assign LEDG[4] = l;
+assign LEDG[5] = l1; 
+assign LEDG[6] = l;
+assign LEDG[7] = l1; 
 
+assign LEDG[0] = l3;
+
+always @(posedge CLOCK_50) begin
+
+	if(KEY[0]==1) begin
+		l3 = 0;
+	end
+
+	if(KEY[0]==0) begin
+		l3 = 1;
+	end
+end
 always @(posedge CLOCK_50) begin
 	contador = contador + 1;
 	
@@ -18,6 +39,11 @@ always @(posedge CLOCK_50) begin
 	if(contador == 0) begin
 		l = ~l;
 	end
+	
+	if(contador == 0) begin
+		l1 = ~l1;
+	end
+	
 end
 
 endmodule
